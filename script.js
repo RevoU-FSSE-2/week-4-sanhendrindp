5c589abb7955b9e871ea3cf1bd549071409b0d3d
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2023-07-05 02:32:14 +0700

    import file from week 3 assignment

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const submitForm = document.querySelector("button");
+
+submitForm.addEventListener("click", function () {
+  alert("UNDER CONSTRUCTION... 🚧👷‍♂️, waiting for next week.");
+});
