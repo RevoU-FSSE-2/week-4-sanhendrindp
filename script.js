33058b758260a573801f7cc19034b10a624bdfdd
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2023-07-05 08:42:27 +0700

    add typed js

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const submitForm = document.querySelector("button");
 submitForm.addEventListener("click", function () {
   alert("UNDER CONSTRUCTION... 🚧👷‍♂️, waiting for next week.");
 });
+
+// TYPED JS
+const typed = new Typed(".multiple-text", {
+  strings: ["Freelancer", "Web Developer", "Gamer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
