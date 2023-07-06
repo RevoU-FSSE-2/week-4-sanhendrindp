982d33f576e96bc3bae68839b04db0d4c3193733
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2023-07-06 10:12:45 +0700

    testing contact form (still not working)

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const submitForm = document.querySelector("button");
+// const submitForm = document.querySelector("button");
 
-submitForm.addEventListener("click", function () {
-  alert("UNDER CONSTRUCTION... 🚧👷‍♂️, waiting for next week.");
-});
+// submitForm.addEventListener("click", function () {
+//   alert("UNDER CONSTRUCTION... 🚧👷‍♂️, waiting for next week.");
+// });
 
 // TYPED JS
 const typed = new Typed(".multiple-text", {
@@ -14,3 +14,24 @@ const typed = new Typed(".multiple-text", {
   backDelay: 1000,
   loop: true,
 });
+
+// SMTP JS
+const sendEmail = function () {
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "fortesting2498@gmail.com",
+    Password: "fortesting12345",
+    To: "fortesting2498@gmail.com",
+    From: document.getElementById("email").value,
+    Subject: "This is the subject",
+    Body:
+      "Name: " +
+      document.getElementById("name").value +
+      "<br> Email: " +
+      document.getElementById("email").value +
+      "<br> Phone no: " +
+      document.getElementById("phone").value +
+      "<br> Message: " +
+      document.getElementById("message").value,
+  }).then((message) => alert("Message sent succesfully!"));
+};
